63c2131e1df5165f3ef8bdd48aa363963c5a7fd7
joy Eugene <joyeugene3@gmail.com>
2026-07-25 19:20:06 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import AdviceCard from './components/AdviceCard'
+
+function App() {
+
+
+  return (
+    <>
+    <main 
+    className="
+    min-h-screen bg-[#202632] flex items-center justify-center
+     px-6 md:px-0
+     ">
+    <AdviceCard />
+    </main>
+    </>
+  )
+}
+export default App
+
---
+++ src/types/advice.ts
@@ -0,0 +1,6 @@
+export interface AdviceResponse {
+    slip :{
+        id: number,
+        advice: string
+    }
+}
